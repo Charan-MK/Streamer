d356f2c1cb330c73350a07c8f107f1d442e74513
Charan MK <charanneeraj98@gmail.com>
2023-05-19 22:07:52 +0530

    adds linter file and resolves linting errors in mongoose.js, videoModels.js and routes.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,3 +1,2 @@
 const mongoose = require('mongoose')
-
 mongoose.connect(process.env.MONGOBD_URL)
--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,6 @@ const app = express()
 const multer = require('multer')
 const Video = require('./db/videoModel')
 require('./db/mongoose')
-const sharp = require('sharp')
 const hbs = require('hbs')
 
 app.use(express.static(path.join(__dirname, '../public')))
@@ -14,62 +13,61 @@ app.use(express.urlencoded())
 app.use(express.json())
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, '../views'))
-hbs.registerPartials(path.join(__dirname,'../views/partials'))
+hbs.registerPartials(path.join(__dirname, '../views/partials'))
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.status(200).render('home')
 })
 
-//fetch the video from Database
-router.get('/db/videos', async (req, res)=>{
-    let dBlist = await Video.find({})
-    
-    let dBvideos = []
-    dBlist.forEach(vid=>{
+// fetch the video from Database
+router.get('/db/videos', async (req, res) => {
+    const dBlist = await Video.find({})
+
+    const dBvideos = []
+    dBlist.forEach(vid => {
         dBvideos.push(vid.name)
     })
-    res.status(200).render('dbList',{
-        dbVideoList:dBvideos
+    res.status(200).render('dbList', {
+        dbVideoList: dBvideos
     })
 })
 
-router.get('/db/videos/:dbVideoName', async (req,res)=>{
+router.get('/db/videos/:dbVideoName', async (req, res) => {
     res.status(200).render('dbVideos', {
         dbVideoName: req.params.dbVideoName
     })
 })
 
-//RETRIEVING MEDIA FROM MONGODB DTATABASE
-router.get('/db/videos/play/:dbVideoName', async (req, res)=>{
-    let video = await Video.find({name:req.params.dbVideoName}) //fetches the specified video array
+// RETRIEVING MEDIA FROM MONGODB DTATABASE
+router.get('/db/videos/play/:dbVideoName', async (req, res) => {
+    const video = await Video.find({ name: req.params.dbVideoName }) // fetches the specified video array
 
-    if(video.length!=0){
-        const dbVideo  =  video[0].video
+    if (video.length !== 0) {
+        const dbVideo = video[0].video
         res.status(200).send(dbVideo)
-    }
-    else{
-        res.status(400).send({"error": "Video could not be found"})
+    } else {
+        res.status(400).send({ error: 'Video could not be found' })
     }
 })
 
 const storage = multer.diskStorage({
-    destination: function(req, file, cb){
+    destination: function (req, file, cb) {
         cb(null, 'assets')
     },
-    filename: function(req, file, cb){
+    filename: function (req, file, cb) {
         cb(null, file.originalname)
     }
 })
 
 const upload = multer({
-    storage: storage,
+    storage,
     limits: {
         fileSize: 100000000
     },
 
-    fileFilter(req, file, callback){
-        if(!file.originalname.match(/\.(mp4|mkv)$/)){
-            callback(new Error("file type must be .mp4 or .mkv"))
+    fileFilter (req, file, callback) {
+        if (!file.originalname.match(/\.(mp4|mkv)$/)) {
+            callback(new Error('file type must be .mp4 or .mkv'))
         }
 
         callback(undefined, true)
@@ -82,83 +80,83 @@ const dBupload = multer({
         fileSize: 15000000
     },
 
-    fileFilter(req, file, callback){
-        if(!file.originalname.match(/\.(mp4|mkv)$/)){
-            callback(new Error("file type must be .mp4 or .mkv"))
+    fileFilter (req, file, callback) {
+        if (!file.originalname.match(/\.(mp4|mkv)$/)) {
+            callback(new Error('file type must be .mp4 or .mkv'))
         }
 
         callback(undefined, true)
     }
 })
 
-router.get('/upload', (req, res)=>{
-     createAssetsDir()
+router.get('/upload', (req, res) => {
+    createAssetsDir()
     res.status(200).render('upload')
 })
 
-router.post('/upload', upload.single('my-video'), (req, res)=>{
-    res.status(200).render("upload_success")
+router.post('/upload', upload.single('my-video'), (req, res) => {
+    res.status(200).render('upload_success')
 })
 
-router.get('/upload/db', (req, res)=>{
+router.get('/upload/db', (req, res) => {
     res.status(200).render('dBupload')
 })
 
-router.post('/upload/db', dBupload.single('my-video'), async (req, res)=>{
+router.post('/upload/db', dBupload.single('my-video'), async (req, res) => {
     const video = new Video({
         name: req.file.originalname,
         video: req.file.buffer
     })
-    let result = video.save()
+    video.save()
     res.status(201).render('upload_success')
 })
 
-function createAssetsDir(){
-    if(!fs.existsSync(path.join(__dirname, '../assets'))){
-        console.log("creating a local directory to store media")
+function createAssetsDir () {
+    if (!fs.existsSync(path.join(__dirname, '../assets'))) {
+        console.log('creating a local directory to store media')
         fs.mkdirSync('./assets')
     }
 }
 
-let videoList =[]
+let videoList = []
 
-router.get('/localVideos', (req, res)=>{
+router.get('/localVideos', (req, res) => {
     createAssetsDir()
     videoList = fs.readdirSync(path.join(__dirname, '../assets'))
-    res.status(200).render('videoList', {videoList})
+    res.status(200).render('videoList', { videoList })
 })
 
-router.get('/video/:videoName', (req, res)=>{
+router.get('/video/:videoName', (req, res) => {
     res.render('videos', {
         videoName: req.params.videoName
     })
 })
 
-router.get('/playVideo/:videoName', (req, res)=>{
-    let range = req.headers.range;
+router.get('/playVideo/:videoName', (req, res) => {
+    let range = req.headers.range
     // console.log("This is range", range)
     if (!range) {
         range = 'bytes=0-'
         // res.status(400).send("Requires Range header");
     }
-    const videoPath = path.join(__dirname, "../assets/"+req.params.videoName);
-    const videoSize = fs.statSync(videoPath).size;
-    const CHUNK_SIZE = 10 ** 6;
-    const start = Number(range.replace(/\D/g, ""));
-    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
-    const contentLength = end - start + 1;
+    const videoPath = path.join(__dirname, '../assets/' + req.params.videoName)
+    const videoSize = fs.statSync(videoPath).size
+    const CHUNK_SIZE = 10 ** 6
+    const start = Number(range.replace(/\D/g, ''))
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1)
+    const contentLength = end - start + 1
     const headers = {
-        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-        "Accept-Ranges": "bytes",
-        "Content-Length": contentLength,
-        "Content-Type": "video/mp4",
-    };
-    res.writeHead(206, headers);
-    const videoStream = fs.createReadStream(videoPath, { start, end });
-    videoStream.pipe(res);
+        'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': contentLength,
+        'Content-Type': 'video/mp4'
+    }
+    res.writeHead(206, headers)
+    const videoStream = fs.createReadStream(videoPath, { start, end })
+    videoStream.pipe(res)
 })
 
 module.exports = {
-    router: router,
-    app: app
+    router,
+    app
 }
